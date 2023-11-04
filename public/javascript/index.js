974574a74bd80ae03ead310e85d87242ccb6f87a
leamonsenego <116014403+leamonsenego@users.noreply.github.com>
2023-11-04 15:40:21 +0100

    Update index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,35 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
 
-  });
+  const characterContainer = document.querySelector(".character-container")
+  const characterCard = document.querySelector(".character-info")
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
 
-  });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+    charactersAPI.getAllCharacters().then(({data : charactersFromApi}) => {
+      charactersFromApi.forEach(character => {
+        const characterCard = generateCharacterCard(character)
+      })
 
-  });
+    })
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+      function generateCharacterCard(name, occupation, isCartoon, weapon){
+    const {name, occupation, isCartoon, weapon} = character
+    const characterCard = document.createElement("div")
+    characterCard.class = "character-info"
+    characterCard.innerHTML = `
+          <div class="name">Name: ${name}</div>
+          <div class="occupation">Occupation: ${occupation}</div>
+          <div class="cartoon">is a Cartoon?: ${isCartoon}</div>
+          <div class="weapon">Weapon: ${weapon}</div>
+        `
+      }
 
-  });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
-  });
-});
+    })})
+
