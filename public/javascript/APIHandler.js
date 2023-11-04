dec5ddce44ac0c9384f2fc6e665c1b236595b484
leamonsenego <116014403+leamonsenego@users.noreply.github.com>
2023-11-04 15:40:51 +0100

    Update APIHandler.js

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,36 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+
+    this.api = axios.create({
+      baseURL: this.BASE_URL,
+    });
   }
 
-  getFullList () {
+  getAllCharacters = () => {
+    return this.api.get("/characters").then(response =>{
+    console.log(response.data)})
+    };
 
-  }
 
-  getOneRegister () {
+  getOneCharacter = (id) => {
+    return this.api.get(`/characters/${id}`).then(response =>{
+      console.log(response.data)
+    })
+  };
 
-  }
+  createCharacter = (characterInfo) => {
+    return this.api.post(`/characters`, characterInfo);
+    console.log(response.data)
+  };
 
-  createOneRegister () {
+  editCharacter = (id, characterInfo) => {
+    return this.api.put(`characters/${id}`, characterInfo);
+    console.log(response.data)
+  };
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteCharacter = (id) => {
+    return this.api.delete(`characters/${id}`);
+    console.log(response.data)
+  };
 }
